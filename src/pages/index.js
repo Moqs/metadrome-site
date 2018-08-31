868d2a45868db983093767e471a9d2e41d08bde9
Moqs <ryoulego@gmail.com>
2018-08-31 14:45:18 +0900

    font test kerning

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,9 +10,9 @@ const IndexPage = () => (
           flexWrap: "wrap",
     }}>
       <figure className="image" style={{marginBottom:"0"}}>
-        <img src={logo} alt="Metadrome" style={{ width: '5rem', marginTop:"0rem", marginBottom:"0", marginLeft:"0", marginRight:"2rem"}} />
+        <img src={logo} alt="Metadrome" style={{ width: '4rem', marginTop:"0rem", marginBottom:"0", marginLeft:"0", marginRight:"2rem"}} />
       </figure>
-      <h1 style={{color:"#F33535" ,fontSize:"5rem",fontFamily: ['Oleo Script', 'Segoe UI', 'sans-serif'], fontKerning:"normal"}}>Metadrome</h1>
+      <h1 style={{color:"#F33535" ,fontSize:"4rem",fontFamily: ['Oleo Script', 'Segoe UI', 'sans-serif'], fontKerning:"normal"}}>Metadrome</h1>
     </div>
     <h1>We've Dreamt Emos.</h1>
     <p>Metadrome（メタドローム）は静的サイトジェネレータ「Gatsby」によって生成され、管理者「<Link to="/about/">もくさい（もく）</Link>」によって実験的に運用されています。</p>
