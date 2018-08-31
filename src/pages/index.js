ed0eb1531d3336c86c084df30d7c66acc7abe542
Moqs <ryoulego@gmail.com>
2018-08-31 14:03:38 +0900

    font test 1

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,9 +9,9 @@ const IndexPage = () => (
           justifyContent:"left"
     }}>
       <figure className="image" style={{marginBottom:"0"}}>
-        <img src={logo} alt="Metadrome" style={{ width: '5rem', marginTop:"0rem", marginBottom:"0", marginLeft:"0", marginRight:"2rem", flexGrow:"5"}} />
+        <img src={logo} alt="Metadrome" style={{ width: '5rem', marginTop:"0rem", marginBottom:"0", marginLeft:"0", marginRight:"2rem"}} />
       </figure>
-      <h1 style={{ color:"#F33535" ,fontSize:"5rem",fontFamily: ['Oleo Script', 'Segoe UI', 'sans-serif'], flexGrow:"5" }}>Metadrome</h1>
+      <h1 style={{fontFeatureSettings:"palt", color:"#F33535" ,fontSize:"5rem",fontFamily: ['Oleo Script', 'Segoe UI', 'sans-serif']}}>Metadrome</h1>
     </div>
     <h1>We've Dreamt Emos.</h1>
     <p>Metadrome（メタドローム）は静的サイトジェネレータ「Gatsby」によって生成され、管理者「<Link to="/about/">もくさい（もく）</Link>」によって実験的に運用されています。</p>
