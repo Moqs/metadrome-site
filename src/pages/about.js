270e3ce2e98d7d412d460d3ea3389d763a4302c9
Moqs <ryoulego@gmail.com>
2018-09-20 14:42:32 +0900

    About page redesigned

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,33 +2,42 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const AboutPage = () => (
-  <div>
+  <div 
+  >
     <div className="block block1">
-      <h1>About “Metadrome”</h1>
-      <p>Metadrome（メタドローム）は静的サイトジェネレータ「Gatsby」によって生成され、管理者「<Link to="/about/">もくさい（もく）</Link>」によって実験的に運用されています。</p>
-      <p>Metadrome is a website generated with a static site generator “Gatsby”, and operated experimentally by owner, Mokusai.</p>
+      <div className="main">
+        <h1>About “Metadrome”</h1>
+        <p>Metadrome（メタドローム）は静的サイトジェネレータ「Gatsby」によって生成され、管理者「<Link to="./#Me">もくさい（もく）</Link>」によって実験的に運用されています。</p>
+        <p>Metadrome is a website generated with a static site generator “Gatsby”, and operated experimentally by owner, Mokusai.</p>
+      </div>
     </div>
-    <div className="block block2">
-      <h1>About Me</h1>
-      <h2>もくさい（もく）・めたどろ</h2>
-      <p>アニメや映画を観て音楽を聴きつつときどき海外ゲームをやりながら家のなかで生きています。<br/>口癖は「家から一歩も出たくない」。</p>
+    <div id="Me" className="block block2">
+      <div className="main">
+        <h1>About Me</h1>
+        <h2>もくさい（もく）・めたどろ</h2>
+        <p>アニメや映画を観て音楽を聴きつつときどき海外ゲームをやりながら家のなかで生きています。<br/>口癖は「家から一歩も出たくない」。</p>
+      </div>
     </div>
     <div className="block block3">
-      <h4>好きなもの</h4>
-      <dl>
-        <dt>映画</dt>
-          <dd>たとえば「ジュラシック・パーク」「インセプション」「レゴ・ムービー」「たまこラブストーリー」……劇場もたまに行きますが、わりとおウチ映画に偏っています。</dd>
-        <dt>アニメ</dt>
-          <dd>たとえば「ゆゆ式」「氷菓」「ヤマノススメ」「宇宙よりも遠い場所」「『アイカツ！』シリーズ」……</dd>
-        <dt>音楽</dt>
-          <dd>ほとんどジャンルを意識しませんが、アニソン以外ではポップ、エレクトロニック、ポスト・ロックを聴くことが多いです。<br/>
-          アニソンではフライングドッグのアーティストをよく聴きます。バンドなら「jizue」「Balmorhea」「Kyte」「きのこ帝国」「JYOCHO (ex-宇宙コンビニ)」……</dd>
-      </dl>
+      <div className="main">
+        <h4>好きなもの</h4>
+        <dl>
+          <dt>映画</dt>
+            <dd>「ジュラシック・パーク」「インセプション」「レゴ・ムービー」「たまこラブストーリー」……</dd>
+          <dt>アニメ</dt>
+            <dd>「ゆゆ式」「氷菓」「ヤマノススメ」「宇宙よりも遠い場所」「『アイカツ！』シリーズ」……</dd>
+          <dt>音楽</dt>
+            <dd>アニソン、ポップ、エレクトロニック、ポスト・ロック……<br/>
+            「jizue」「Balmorhea」「Kyte」「きのこ帝国」「JYOCHO (ex-宇宙コンビニ)」……</dd>
+        </dl>
+      </div>
     </div>
     <div className="block block4">
-      <h1>Contact</h1>
-      <p>Email: moku@metadro.me</p>
-        <Link to="/">Go back to the homepage</Link>
+      <div className="main">
+        <h1>Contact</h1>
+        <p>Email: moku@metadro.me</p>
+          <Link to="/">Go back to the homepage</Link>
+        </div>
     </div>
   </div>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,14 +3,16 @@ import Link from 'gatsby-link'
 import logo from '../img/logo.svg'
 
 const IndexPage = () => (
-  <div>
-    <div style={{display:"flex",
+  <div className="main">
+    <div style={{
+          //display:"flex",
           //alignItems:"center",
-          justifyContent:"center",
-          flexWrap: "wrap",
+          //justifyContent:"center",
+          //flexWrap: "wrap",
+          textAlign:"center"
     }}>
       <figure className="image" style={{marginBottom:"0"}}>
-        <img src={logo} alt="Metadrome" style={{ width: '4rem', marginTop:"0rem", marginBottom:"0", marginLeft:"2rem", marginRight:"2rem"}} />
+        <img src={logo} alt="Metadrome" style={{ width: '6rem', marginTop:"0rem", marginBottom:"0", marginLeft:"2rem", marginRight:"2rem"}} />
       </figure>
       <h1 style={{color:"#F33535" ,fontSize:"4rem",fontFamily: ['Oleo Script', 'Segoe UI', 'sans-serif'], fontKerning:"normal"}}>Metadrome</h1>
     </div>
